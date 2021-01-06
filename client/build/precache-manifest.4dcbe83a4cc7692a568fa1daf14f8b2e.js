4203a79776be51001e59c4ab91c7920dae32d32c
dbvora03 <54694188+dbvora03@users.noreply.github.com>
2021-01-05 23:59:14 -0700

    this is your final warning

---
+++ client/build/precache-manifest.4dcbe83a4cc7692a568fa1daf14f8b2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4be907c4a369e8a5ea8c32af3d5e7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5981cccb214311038878",
+    "url": "/static/css/main.1403fbc1.chunk.css"
+  },
+  {
+    "revision": "4bf97438bcf4acb52efe",
+    "url": "/static/js/2.9d8419d3.chunk.js"
+  },
+  {
+    "revision": "b013b7cd7a2da92c16a2d9932cfeb76c",
+    "url": "/static/js/2.9d8419d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5981cccb214311038878",
+    "url": "/static/js/main.015daea8.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.use(express.static('client/build'))
 
 app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'client','build', 'index.html')));
 */
+
 app.use(express.static(path.join('/client/build/index.html')));
 
 app.get('*', (req, res) => {
