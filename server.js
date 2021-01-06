fb3a0cd4d9ea854db921ff5baaefe3332cc0954d
dbvora03 <54694188+dbvora03@users.noreply.github.com>
2021-01-05 23:53:19 -0700

    this is your final warning

--- server.js
+++ server.js
@@ -41,11 +41,10 @@ app.use(express.static('client/build'))
 
 app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'client','build', 'index.html')));
 */
-
-app.use(express.static(path.join(__dirname, './client/build/index.html')));
+app.use(express.static(path.join('/client/build/index.html')));
 
 app.get('*', (req, res) => {
-res.sendFile(path.join((__dirname, './client/build/index.html')));
+res.sendFile(path.join(('/client/build/index.html')));
 });
 
 // Allows the server to use static files during production
