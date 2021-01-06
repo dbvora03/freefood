be1d90c95b2c384cf5646ef19c4b8e59b760736f
dbvora03 <54694188+dbvora03@users.noreply.github.com>
2021-01-05 23:24:15 -0700

    i just want to go to sleep

--- server.js
+++ server.js
@@ -35,12 +35,12 @@ app.use(express.json())
 app.use(require("./routes/auth"))
 app.use(require("./routes/post"))
 
+
+app.use(express.static('client/build'))
 const path = require("path")
 if(process.env.NODE_ENV=="production") {
-    app.use(express.static('client/build'))
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'client','build', 'index.html')));
 }
-
 
 app.get('*', (req, res) => res.sendFile(path.resolve('client/build', 'index.html')));
 
