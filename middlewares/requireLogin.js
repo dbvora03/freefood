dfa47abd354706cc703097cdc63247d1d0bf2707
dbvora03 <54694188+dbvora03@users.noreply.github.com>
2021-01-06 00:35:06 -0700

    process env

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -15,7 +15,7 @@ module.exports = (req, res, next)=> {
         const token = authorization.replace("Bearer ", "")
 
         //Checks to see if the tokens are matched and not tampered with
-        jwt.verify(token,JWT_SECRET,(err,payload)=>{
+        jwt.verify(token,"hi",(err,payload)=>{
             if(err) {
                 return res.status(401).json({error: "you must be logged in"})
             }
