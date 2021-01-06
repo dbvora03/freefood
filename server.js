6dbf93eeac22e34505eecf7fe32604428b142823
dbvora03 <54694188+dbvora03@users.noreply.github.com>
2021-01-05 23:12:57 -0700

    please, please, please

--- server.js
+++ server.js
@@ -38,11 +38,11 @@ app.use(require("./routes/post"))
 const path = require("path")
 if(process.env.NODE_ENV=="production") {
     app.use(express.static('client/build'))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'client','build', 'index.html')));
+    app.get('/signin', (req, res) => res.sendFile(path.resolve(__dirname,'client','build', 'index.html')));
 }
 
 
-app.get('*', (req, res) => res.sendFile(path.resolve('client/build', 'index.html')));
+app.get('/signin', (req, res) => res.sendFile(path.resolve('client/build', 'index.html')));
 
 
 // Allows the server to use static files during production
